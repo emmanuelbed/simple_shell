b5ecf8fc6293adfeb993b8dc24fe55e42df9fe75
emmanuelbed <bedzoemmanuel8@gmail.com>
2023-07-23 02:20:57 -0700

    Creating the atoi file

---
+++ _atoi.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+
+/**
+ * interactive - Checks if the shell is running in interactive mode.
+ * @info: Pointer to the info_t struct.
+ *
+ * Return: 1 if running in interactive mode, 0 otherwise.
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - Checks if a character is a delimiter.
+ * @c: The character to check.
+ * @delim: The delimiter string.
+ *
+ * Return: 1 if the character is a delimiter, 0 otherwise.
+ */
+int is_delim(char c, char *delim)
+{
+	for (; *delim; delim++)
+	{
+		if (*delim == c)
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to check.
+ *
+ * Return: 1 if the character is alphabetic, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: 0 if there are, the converted number otherwise.
+ */
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, out;
+	unsigned int result = 0;
+
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		out = -result;
+	else
+		out = result;
+
+	return (out);
+}
+
