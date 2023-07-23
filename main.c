19e0cb539c63e85983b80c3b4d913c3e8dadca7a
emmanuelbed <bedzoemmanuel8@gmail.com>
2023-07-23 03:40:29 -0700

    Added the main file

---
+++ main.c
@@ -0,0 +1,48 @@
+#include "shell.h"
+
+/**
+ * main - Entry point of the program.
+ * @ac: Argument count.
+ * @av: Argument vector.
+ *
+ * Return: 0 on success, 1 on error.
+ */
+int main(int ac, char **av)
+{
+	info_t info[] = {INFO_INIT};
+	int fd = 2;
+
+	asm("mov %1, %0\n\t"
+	    "add $3, %0"
+	    : "=r"(fd)
+	    : "r"(fd));
+
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			switch (errno)
+			{
+				case EACCES:
+					exit(126);
+				case ENOENT:
+					_eputs(av[0]);
+					_eputs(": 0: Can't open ");
+					_eputs(av[1]);
+					_eputchar('\n');
+					_eputchar(BUF_FLUSH);
+					exit(127);
+				default:
+					return (EXIT_FAILURE);
+			}
+		}
+		info->readfd = fd;
+	}
+
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
+}
+
